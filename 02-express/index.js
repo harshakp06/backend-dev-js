57dbc478480223d5487835af2bf75176ecf5b6eb
Harsha Vardhan <kpharsha@yahoo.com>
2024-09-10 13:38:39 +0530

    env file adding

--- 02-express/index.js
+++ 02-express/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import 'dotenv/config'
+// require('dotenv').config()
 
 const app = express()
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 // app.get("/",(req,res) => {
 //     res.send('Hello from Harsha - using Express library')
